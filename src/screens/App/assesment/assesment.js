fbce42436fab53af94e0ad24b47583264d129e58
WAQAS AHMED <80480129+WAQAZ0178@users.noreply.github.com>
2024-07-05 10:11:46 +0500

    after addingg test cases for result and splash screen

--- src/screens/App/assesment/assesment.js
+++ src/screens/App/assesment/assesment.js
@@ -1,150 +1,142 @@
-import React from "react";
-import { render, fireEvent, waitFor } from "@testing-library/react-native";
-import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
-import Assesment from "../path/to/Assesment";
-import { NavigationContainer } from "@react-navigation/native";
+import React, { useCallback, useState } from "react";
+import { View, StatusBar, SafeAreaView, FlatList } from "react-native";
+import styles from "./styles";
+import { useDispatch, useSelector } from "react-redux";
+import { OptionCard } from "../../../components/feed";
+import { colors } from "../../../global/utilities";
+import { AppButton, AuthHeader } from "../../../components/general";
 import { ACTIONS } from "../../../store/actions/question";
 
-jest.mock("@react-navigation/native", () => {
-  const actualNav = jest.requireActual("@react-navigation/native");
-  return {
-    ...actualNav,
-    useNavigation: () => ({
-      replace: jest.fn(),
-    }),
+const Assesment = (props) => {
+  const { navigation } = props;
+  const list = useSelector((state) => state?.state?.reducer?.questionList);
+  const [question, setQuestions] = useState(list);
+  const [currentIndex, setcurrentIndex] = useState(0);
+  const dispatch = useDispatch();
+
+  const onPressOption = useCallback(
+    (item, index) => {
+      setQuestions((prevQuestions) => {
+        const updatedQuestions = prevQuestions.map((question, qIndex) => {
+          if (qIndex === currentIndex) {
+            const updatedOptions = question.options.map((option, oIndex) => ({
+              ...option,
+              isSelected: oIndex === index,
+            }));
+            return { ...question, options: updatedOptions };
+          }
+          return question;
+        });
+
+        dispatch(ACTIONS.setQuestionList(updatedQuestions));
+        return updatedQuestions;
+      });
+    },
+    [currentIndex]
+  );
+
+  const onClickNext = () => {
+    if (currentIndex < question?.length - 1) {
+      setcurrentIndex(currentIndex + 1);
+    }
   };
-});
 
-describe("Assesment Component", () => {
-  const mockStore = configureStore([]);
-  let store;
-  let navigation;
+  const onClickPrevious = () => {
+    if (currentIndex > 0) {
+      setcurrentIndex(currentIndex - 1);
+    }
+  };
 
-  beforeEach(() => {
-    store = mockStore({
-      state: {
-        reducer: {
-          questionList: [
-            {
-              id: 1,
-              question: "How would you describe your investment knowledge?",
-              options: [
-                { option: "Novice", score: 1, isSelected: false },
-                { option: "Intermediate", score: 2, isSelected: false },
-                { option: "Advanced", score: 3, isSelected: false },
-              ],
-            },
-            {
-              id: 2,
-              question: "Investment Duration",
-              options: [
-                {
-                  option: "Short-term (less than 1 year)",
-                  score: 1,
-                  isSelected: false,
-                },
-                {
-                  option: "Medium-term (1-5 years)",
-                  score: 2,
-                  isSelected: false,
-                },
-                {
-                  option: "Long-term (more than 5 years)",
-                  score: 3,
-                  isSelected: false,
-                },
-              ],
-            },
-          ],
-        },
-      },
-    });
+  const handleFinish = () => {
+    // Calculate the total score
+    const totalScore = question.reduce((sum, question) => {
+      const selectedOption = question.options.find(
+        (option) => option.isSelected
+      );
+      return sum + (selectedOption ? selectedOption.score : 0);
+    }, 0);
 
-    navigation = require("@react-navigation/native").useNavigation();
-  });
+    // Determine the profile based on the total score
+    let profile = "";
+    if (totalScore >= 5 && totalScore <= 8) {
+      profile = "🛡️ Conservative: Safe and steady.";
+    } else if (totalScore >= 9 && totalScore <= 12) {
+      profile = "⚖️ Balanced: A mix of safety and growth.";
+    } else if (totalScore >= 13) {
+      profile = "🚀 Aggressive: Woohoo! You take big risks!";
+    }
 
-  test("renders correctly", () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <NavigationContainer>
-          <Assesment navigation={navigation} />
-        </NavigationContainer>
-      </Provider>
+    // Navigate to the result screen and pass the score and profile
+    navigation.replace("Result", { totalScore, profile });
+  };
+
+  const renderOption = (item, index) => {
+    return (
+      <OptionCard
+        onClickOption={() => onPressOption(item, index)}
+        item={item}
+      />
     );
+  };
 
-    expect(
-      getByText("Q 1 : How would you describe your investment knowledge?")
-    ).toBeTruthy();
-  });
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor={colors.white}
+        barStyle={"dark-content"}
+        translucent={false}
+      />
+      <AuthHeader
+        onPress={{}}
+        title={`Q ${currentIndex + 1} : ${question[currentIndex]?.question}`}
+      />
 
-  test("updates selected option and dispatches action", () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <NavigationContainer>
-          <Assesment navigation={navigation} />
-        </NavigationContainer>
-      </Provider>
-    );
+      <View style={styles.innerContainer}>
+        <FlatList
+          data={question[currentIndex]?.options}
+          renderItem={({ item, index }) => renderOption(item, index)}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+      <View style={styles.bottomButtonContainer}>
+        {currentIndex > 0 && (
+          <AppButton
+            backgroundColor={colors.black}
+            disabled={false}
+            loading={false}
+            loaderColor={colors.white}
+            marginTop={0}
+            onPress={() => onClickPrevious()}
+            title="Previous"
+            width={40}
+          />
+        )}
+        {currentIndex < question?.length - 1 ? (
+          <AppButton
+            backgroundColor={colors.black}
+            disabled={false}
+            loading={false}
+            loaderColor={colors.white}
+            marginTop={0}
+            onPress={() => onClickNext()}
+            title="Next"
+            width={currentIndex > 0 ? 40 : 95}
+          />
+        ) : (
+          <AppButton
+            backgroundColor={colors.black}
+            disabled={false}
+            loading={false}
+            loaderColor={colors.white}
+            marginTop={0}
+            onPress={() => handleFinish()}
+            title="Finish"
+            width={40}
+          />
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
 
-    const noviceOption = getByText("Novice");
-    fireEvent.press(noviceOption);
-
-    const actions = store.getActions();
-    expect(actions).toEqual([
-      {
-        type: ACTIONS.setQuestionList.type,
-        payload: expect.anything(),
-      },
-    ]);
-  });
-
-  test("navigates correctly on clicking Next and Previous buttons", () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <NavigationContainer>
-          <Assesment navigation={navigation} />
-        </NavigationContainer>
-      </Provider>
-    );
-
-    const nextButton = getByText("Next");
-    fireEvent.press(nextButton);
-    expect(getByText("Q 2 : Investment Duration")).toBeTruthy();
-
-    const previousButton = getByText("Previous");
-    fireEvent.press(previousButton);
-    expect(
-      getByText("Q 1 : How would you describe your investment knowledge?")
-    ).toBeTruthy();
-  });
-
-  test("calculates total score and profile on finish", async () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <NavigationContainer>
-          <Assesment navigation={navigation} />
-        </NavigationContainer>
-      </Provider>
-    );
-
-    const intermediateOption = getByText("Intermediate");
-    fireEvent.press(intermediateOption);
-
-    const nextButton = getByText("Next");
-    fireEvent.press(nextButton);
-
-    const mediumTermOption = getByText("Medium-term (1-5 years)");
-    fireEvent.press(mediumTermOption);
-
-    const finishButton = getByText("Finish");
-    fireEvent.press(finishButton);
-
-    await waitFor(() =>
-      expect(navigation.replace).toHaveBeenCalledWith("Result", {
-        totalScore: 4, // Adjust the score as per the selected options
-        profile: "⚖️ Balanced: A mix of safety and growth.", // Adjust the profile as per the score
-      })
-    );
-  });
-});
+export default Assesment;
